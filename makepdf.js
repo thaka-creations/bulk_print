be3429bcea3a5938c529016feb64c9efd5e0c04b
Abdul Nelfrank <nelfrankaj@gmail.com>
2024-02-22 22:56:31 +0300

    initial commit

---
+++ makepdf.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const pdfMake = require('pdfmake');
+
+function createPdf(data, outputFilename) {
+    const fonts = {
+        Roboto: {
+            normal: 'node_modules/roboto-font/fonts/Roboto/roboto-bold-webfont.ttf',
+            bold: 'node_modules/roboto-font/fonts/Roboto/roboto-bold-webfont.ttf',
+            italics: 'node_modules/roboto-font/fonts/Roboto/roboto-bold-webfont.ttf',
+            bolditalics: 'node_modules/roboto-font/fonts/Roboto/roboto-bold-webfont.ttf',
+        },
+    };
+
+    const printer = new pdfMake(fonts);
+
+    const docDefinition = {
+        content: [],
+    };
+
+    // Add data to the PDF
+    for (const key in data) {
+        if (data.hasOwnProperty(key)) {
+            docDefinition.content.push(`${key}: ${data[key]}`);
+        }
+    }
+
+    // Create PDF
+    const pdfDoc = printer.createPdfKitDocument(docDefinition);
+    pdfDoc.pipe(fs.createWriteStream(outputFilename));
+    pdfDoc.end();
+
+    console.log(`PDF created: ${outputFilename}`);
+}
+
+function batchPrintFromJson(jsonData) {
+    jsonData.forEach((data, index) => {
+        const outputFilename = `output_${index + 1}.pdf`;
+        createPdf(data, outputFilename);
+    });
+}
+
+// Example JSON data
+const jsonData = [
+    { Name: 'John Doe', Age: 25, City: 'Example City' },
+    { Name: 'Jane Smith', Age: 30, City: 'Another City' },
+    // Add more data as needed
+];
+
+// Print PDFs from JSON data
+batchPrintFromJson(jsonData);
---
+++ print.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+const { exec } = require('child_process');
+
+const printFile = async (docUrl) => {
+    try {
+        const response = await axios.get(docUrl, { responseType: 'arraybuffer' });
+
+        // Convert to buffer -> rep binary content of the file
+        const dataBuffer = Buffer.from(response.data);
+
+        // Use lp command to print the data -> create child process
+        const lpCommand = `lp -`;
+        const lpProcess = exec(lpCommand, (error, stdout, stderr) => {
+            if (error) {
+                console.error('Error printing file:', error.message);
+            } else {
+                console.log('File sent to print successfully.');
+            }
+        });
+
+        lpProcess.stdin.write(dataBuffer); //transfer data to stdin of lp process
+        lpProcess.stdin.end(); // close the stream to signal end of the data
+    } catch (error) {
+        console.error('Error fetching file:', error.message);
+    }
+};
+
+// Example usage
+const fileLinks = [
+    'http://192.168.214.200/sharedservice/media/2024/02/22/18/bf3a3889-b54e-43a9-b93e-14978f9247ca.pdf',
+    'https://css4.pub/2017/newsletter/drylab.pdf',
+    'https://www.princexml.com/samples/invoice/invoicesample.pdf'
+];
+
+fileLinks.forEach((fileLink) => {
+    printFile(fileLink);
+});
